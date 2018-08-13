c377a185b36bcfb7923c3ccb67a4c24c82e5b8e7
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-08-13 18:35:43 +0100

    Creating LINQ statement that groups students and counts those who have enrolled in a class

--- ContosoUniversity/Pages/About.cshtml.cs
+++ ContosoUniversity/Pages/About.cshtml.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoUniversity.Models;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Pages
 {
     public class AboutModel : PageModel
     {
-        public string Message { get; set; }
+		private readonly SchoolContext _context;
 
-        public void OnGet()
-        {
-            Message = "Your application description page.";
-        }
+		public AboutModel(SchoolContext context)
+		{
+			_context = context;
+		}
+
+		public IList<EnrollmentDateGroup> Student { get; set; }
+
+        public async Task OnGetAsync()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Student
+				group student by student.EnrollmentDate into dateGroup
+				select new EnrollmentDateGroup()
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+
+			Student = await data.AsNoTracking().ToListAsync();
+		}
     }
 }
